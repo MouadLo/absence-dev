5992b1bdca276b737b79c51e360aa390975189c2
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2020-08-20 16:03:56 -0400

    change security

--- src/main/java/com/miage/app/security/AuthoritiesConstants.java
+++ src/main/java/com/miage/app/security/AuthoritiesConstants.java
@@ -4,13 +4,13 @@ package com.miage.app.security;
  * Constants for Spring Security authorities.
  */
 public final class AuthoritiesConstants {
+  public static final String ADMIN = "ROLE_ADMIN";
 
-    public static final String ADMIN = "ROLE_ADMIN";
+  public static final String USER = "ROLE_USER";
 
-    public static final String USER = "ROLE_USER";
+  public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
-    public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+  public static final String MANAGER = "ROLE_MANAGER";
 
-    private AuthoritiesConstants() {
-    }
+  private AuthoritiesConstants() {}
 }
